9713cee683881029968c7f46becfdf2307fbec96
Michael <michaelgrisso2011@gmail.com>
2022-08-29 12:18:56 -0400

    readme update

--- README.md
+++ README.md
@@ -11,6 +11,8 @@
       <ul>
         <li><a href="#built-with">Built With</a></li>
       </ul>
+      <a href="#getting-started">Getting Started</a>
+    </li>
     </li>
     <li><a href="#acknowledgments">Acknowledgments</a></li>
   </ol>
@@ -22,16 +24,32 @@
 
 <!--this is just a quick snippet, but we can fine tune it later.-->
 
-Aware is an easy-to-use single page web application. It is designed to enter "gluten", "fish", "celery", or "milk", and then remove each all dog food products with those ingredients from the results.
+Aware is an easy-to-use single page reverse-search application built for our Mod 3 school project. Users can enter an ingredient their dog cannot consume and the results provide all dog foods available without the allergen.
 
 ### Built With
 
 - [![Bootstrap][bootstrap.com]][bootstrap-url]
-- ![CSS.com]
-- ![HTML.com]
-- [JavaScript](https://github.com/Michael-Grisso/aware-dog-food/tree/main/assets/js)
+- [![CSS][css.com]][css-url]
+- [![HTML][html.com]][html-url]
+- [![JavaScript][javascript.com]][javascript-url]
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+### Getting Started
+
+You can test out this project locally by cloning the repository.
+
+```sh
+git clone https://github.com/Michael-Grisso/aware-dog-food.git
+```
+
+### Usage
+
+We tried make the UX as easy as possible. Here are a few quick examples on how to use:
+
+![](assets/img/Usage-Step-1.jpg "How to Enter Food Allergen")
+
+Then:
+
+![](assets/img/Usage-Step-2.jpg "How to Enter Food Allergen")
 
 <!-- LICENSE -->
 
@@ -39,22 +57,24 @@ Aware is an easy-to-use single page web application. It is designed to enter "gl
 
 Distributed under the MIT License. See `LICENSE.txt` for more information.
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
 <!-- ACKNOWLEDGMENTS -->
 
 ### Acknowledgments
 
-- [othneildrew Best README Template](https://github.com/othneildrew/Best-README-Template)
 - [adam-p Markdown Language Cheat Sheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
 - [GitHub docs](https://docs.github.com/en)
+- [MDN Fetch API Docs](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
+- [othneildrew Best README Template](https://github.com/othneildrew/Best-README-Template)
+- [Stack Overflow](https://stackoverflow.com/)
+- [W3 Schools](https://www.w3schools.com/)
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+<!-- HELP LINKS & IMAGES -->
 
-<!-- MARKDOWN LINKS & IMAGES -->
-<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
-
-[bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
+[bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=bootstrap&logoColor=white
 [bootstrap-url]: https://bootstrapmade.com/regna-bootstrap-onepage-templat
-[css.com]: https://img.shields.io/badge/CSS-FF0000?style=for-the-badge&logo=tailwind-css&logoColor=white
-[html.com]: https://img.shields.io/badge/HTML-302683?style=for-the-badge&logo=HTML%20Academy&logoColor=white
+[css.com]: https://img.shields.io/badge/CSS-FF0000?style=css&logoColor=white
+[css-url]: https://developer.mozilla.org/en-US/docs/Web/CSS
+[html.com]: https://img.shields.io/badge/HTML-302683?style=HTML%20Academy&logoColor=white
+[html-url]: https://www.w3schools.com/html/
+[javascript.com]: https://img.shields.io/badge/JavaScript-FFD700?style=javascript&logoColor=black
+[javascript-url]: https://developer.mozilla.org/en-US/docs/Web/JavaScript
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,277 +5,271 @@
  * License: https://bootstrapmade.com/license/
  */
 (function () {
-	"use strict";
+  "use strict";
 
-	/**
-	 * Easy selector helper function
-	 */
-	const select = (el, all = false) => {
-		el = el.trim();
-		if (all) {
-			return [...document.querySelectorAll(el)];
-		} else {
-			return document.querySelector(el);
-		}
-	};
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
 
-	/**
-	 * Easy event listener function
-	 */
-	const on = (type, el, listener, all = false) => {
-		let selectEl = select(el, all);
-		if (selectEl) {
-			if (all) {
-				selectEl.forEach((e) => e.addEventListener(type, listener));
-			} else {
-				selectEl.addEventListener(type, listener);
-			}
-		}
-	};
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
 
-	/**
-	 * Easy on scroll event listener
-	 */
-	const onscroll = (el, listener) => {
-		el.addEventListener("scroll", listener);
-	};
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
 
-	/**
-	 * Navbar links active state on scroll
-	 */
-	let navbarlinks = select("#navbar .scrollto", true);
-	const navbarlinksActive = () => {
-		let position = window.scrollY + 200;
-		navbarlinks.forEach((navbarlink) => {
-			if (!navbarlink.hash) return;
-			let section = select(navbarlink.hash);
-			if (!section) return;
-			if (
-				position >= section.offsetTop &&
-				position <= section.offsetTop + section.offsetHeight
-			) {
-				navbarlink.classList.add("active");
-			} else {
-				navbarlink.classList.remove("active");
-			}
-		});
-	};
-	window.addEventListener("load", navbarlinksActive);
-	onscroll(document, navbarlinksActive);
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
 
-	/**
-	 * Scrolls to an element with header offset
-	 */
-	const scrollto = (el) => {
-		let header = select("#header");
-		let offset = header.offsetHeight;
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
 
-		if (!header.classList.contains("header-scrolled")) {
-			offset -= 20;
-		}
+    if (!header.classList.contains("header-scrolled")) {
+      offset -= 20;
+    }
 
-		let elementPos = select(el).offsetTop;
-		window.scrollTo({
-			top: elementPos - offset,
-			behavior: "smooth",
-		});
-	};
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
+    });
+  };
 
-	/**
-	 * Toggle .header-scrolled class to #header when page is scrolled
-	 */
-	let selectHeader = select("#header");
-	if (selectHeader) {
-		const headerScrolled = () => {
-			if (window.scrollY > 100) {
-				selectHeader.classList.add("header-scrolled");
-			} else {
-				selectHeader.classList.remove("header-scrolled");
-			}
-		};
-		window.addEventListener("load", headerScrolled);
-		onscroll(document, headerScrolled);
-	}
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+      }
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+  }
 
-	/**
-	 * Mobile nav toggle
-	 */
-	on("click", ".mobile-nav-toggle", function (e) {
-		select("#navbar").classList.toggle("navbar-mobile");
-		this.classList.toggle("bi-list");
-		this.classList.toggle("bi-x");
-	});
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
-	/**
-	 * Scrool with ofset on links with a class name .scrollto
-	 */
-	on(
-		"click",
-		".scrollto",
-		function (e) {
-			if (select(this.hash)) {
-				e.preventDefault();
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
-				let navbar = select("#navbar");
-				if (navbar.classList.contains("navbar-mobile")) {
-					navbar.classList.remove("navbar-mobile");
-					let navbarToggle = select(".mobile-nav-toggle");
-					navbarToggle.classList.toggle("bi-list");
-					navbarToggle.classList.toggle("bi-x");
-				}
-				scrollto(this.hash);
-			}
-		},
-		true
-	);
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
 
-	/**
-	 * Scroll with ofset on page load with hash links in the url
-	 */
-	window.addEventListener("load", () => {
-		if (window.location.hash) {
-			if (select(window.location.hash)) {
-				scrollto(window.location.hash);
-			}
-		}
-	});
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
 
-	/**
-	 * Porfolio isotope and filter
-	 */
-	window.addEventListener("load", () => {
-		let portfolioContainer = select(".portfolio-container");
-		if (portfolioContainer) {
-			let portfolioIsotope = new Isotope(portfolioContainer, {
-				itemSelector: ".portfolio-item",
-				layoutMode: "fitRows",
-			});
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
+      });
 
-			let portfolioFilters = select("#portfolio-flters li", true);
+      let portfolioFilters = select("#portfolio-flters li", true);
 
-			on(
-				"click",
-				"#portfolio-flters li",
-				function (e) {
-					e.preventDefault();
-					portfolioFilters.forEach(function (el) {
-						el.classList.remove("filter-active");
-					});
-					this.classList.add("filter-active");
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-					portfolioIsotope.arrange({
-						filter: this.getAttribute("data-filter"),
-					});
-					portfolioIsotope.on("arrangeComplete", function () {
-						AOS.refresh();
-					});
-				},
-				true
-			);
-		}
-	});
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
+    }
+  });
 
-	/**
-	 * Initiate portfolio lightbox
-	 */
-	const portfolioLightbox = GLightbox({
-		selector: ".portfolio-lightbox",
-	});
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: ".portfolio-lightbox",
+  });
 
-	/**
-	 * Portfolio details slider
-	 */
-	new Swiper(".portfolio-details-slider", {
-		speed: 400,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: ".swiper-pagination",
-			type: "bullets",
-			clickable: true,
-		},
-	});
+  /**
+   * Portfolio details slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
 
-	/**
-	 * Animation on scroll
-	 */
-	window.addEventListener("load", () => {
-		AOS.init({
-			duration: 1000,
-			easing: "ease-in-out",
-			once: true,
-			mirror: false,
-		});
-	});
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
 
-	/**
-	 * Initiate Pure Counter
-	 */
-	new PureCounter();
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
 })();
 
-
 function searchFood() {
-	let inputAllergen = document.getElementById("inputForSearch").value;
-	// let UPCresultBox = document.getElementById("searchResultUPCcodes");
-	let searchResultArea = document.getElementById("searchResultsArea")
-	
+  let inputAllergen = document.getElementById("inputForSearch").value;
+  // let UPCresultBox = document.getElementById("searchResultUPCcodes");
+  let searchResultArea = document.getElementById("searchResultsArea");
 
-	let url = `https://us.openpetfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=dog_food&json=true&tagtype_1=allergens&tag_contains_1=without&tag_1=${inputAllergen}`;
-	// console.log(url);
-	fetch(url)
-	.then(resp =>resp.json())
-	.then(data =>{
-		
-		 //console.log(data)
+  let url = `https://us.openpetfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=dog_food&json=true&tagtype_1=allergens&tag_contains_1=without&tag_1=${inputAllergen}`;
+  // console.log(url);
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      //console.log(data)
 
-		
-		for (let product of data.products) {
-			let productName = product.product_name
-			let productImage = product.image_url
-			let ingredientList = product.ingredients
-				console.log(ingredientList)		//	
-			
+      for (let product of data.products) {
+        let productName = product.product_name;
+        let productImage = product.image_url;
+        let ingredientList = product.ingredients;
+        console.log(ingredientList); //
 
-			let resultBox = document.createElement("div");
-			resultBox.setAttribute("class", "col-lg-4 col-md-6 portfolio-item filter-app")
+        let resultBox = document.createElement("div");
+        resultBox.setAttribute(
+          "class",
+          "col-lg-4 col-md-6 portfolio-item filter-app"
+        );
 
-			let result = document.createElement("h4")
-			result.innerText = productName
-			resultBox.append(result)
+        let result = document.createElement("h4");
+        result.innerText = productName;
+        resultBox.append(result);
 
-			let pic = document.createElement("img")
-			pic.setAttribute("src", productImage)
-			pic.setAttribute("class", "img-fluid")
-			resultBox.append(pic)
+        let pic = document.createElement("img");
+        pic.setAttribute("src", productImage);
+        pic.setAttribute("class", "img-fluid");
+        resultBox.append(pic);
 
-			searchResultArea.appendChild(resultBox)
-
-		}
-		
-	})
-	.catch(err => console.error(err));
+        searchResultArea.appendChild(resultBox);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
-
 function dogFacts() {
-	let urlDog = "https://dog-api.kinduff.com/api/facts";
-	
-	// console.log(url);
-	fetch(urlDog)
-	.then(resp => {
-		console.log(resp)
-	})
+  let urlDog = "https://dog-api.kinduff.com/api/facts";
 
-	//have not got to next promise
-	// .then(data =>{
-	// 	let dogFacts = data.dogFacts
-	// 	// console.log(dogFacts)
-		
+  // console.log(url);
+  fetch(urlDog)
+    .then((resp) => {
+      console.log(resp);
+    })
 
-	// })
-	.catch(err => console.error(err));
+    //have not got to next promise
+    // .then(data =>{
+    // 	let dogFacts = data.dogFacts
+    // 	// console.log(dogFacts)
+
+    // })
+    .catch((err) => console.error(err));
 }
